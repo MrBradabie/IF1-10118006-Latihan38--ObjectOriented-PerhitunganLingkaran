9b558c78c10efa9ec359d43fb75a676404d12899
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-10-17 20:12:30 +0700

    Add files via upload

---
+++ IF1-10118006-Latihan38-(ObjectOriented)PerhitunganLingkaran/src/if1/pkg10118006/latihan38/objectoriented/perhitunganlingkaran/IF110118006Latihan38ObjectOrientedPerhitunganLingkaran.java
@@ -0,0 +1,19 @@
+package if1.pkg10118006.latihan38.objectoriented.perhitunganlingkaran;
+
+/*
+ * NAMA              : Alfarizi Rizki Pane
+ * KELAS             : IF-01
+ * NIM               : 10118006
+ */
+public class IF110118006Latihan38ObjectOrientedPerhitunganLingkaran {
+
+    public static void main(String[] args) {
+        
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
